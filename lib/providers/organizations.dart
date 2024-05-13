150f6bc7ea143f127a7bfe8709d70e41883763b4
John Rufo de la Peña <jodelapena2@up.edu.ph>
2024-05-13 15:41:55 +0800

    Change provider to return a Future

--- lib/providers/organizations.dart
+++ lib/providers/organizations.dart
@@ -3,20 +3,28 @@ import 'dart:collection';
 import 'package:final_proj/entities/organization.dart';
 import 'package:flutter/material.dart';
 
+/// Provides access to a collection of [Organization]s that can be updated.
 abstract class Organizations with ChangeNotifier {
-  /// Returns the number of pages of organizations in the list if each page contained [pageSize] organizations
-  int getPageCount(int pageSize);
+  /// Returns the number of pages of organizations in the collection if each page contained
+  /// [pageSize] organizations.
+  Future<int> getPageCount(int pageSize);
 
-  UnmodifiableListView<Organization> getOrganizations(
+  /// Returns the organizations on page [pageNumber] of the collection if each page contained
+  /// [pageSize] organizations.
+  Future<UnmodifiableListView<Organization>> getOrganizations(
     int pageNumber,
     int pageSize,
   );
 
-  void add(Organization organization);
+  /// Adds [organization] to the collection.
+  Future<void> add(Organization organization);
 
-  void remove(Organization organization);
+  /// Removes [organization] from the list.
+  Future<void> remove(Organization organization);
 
-  void update(Organization organization);
+  /// Updates [organization] in the list. An organization with the same ID as [organization] must
+  /// exist in the collection for it to be updated. Otherwise, the collection will be unchanged.
+  Future<void> update(Organization organization);
 }
 
 final class ListOrganizations extends Organizations {
@@ -36,46 +44,60 @@ final class ListOrganizations extends Organizations {
   ];
 
   @override
-  int getPageCount(int pageSize) {
-    return (_organizations.length / pageSize).ceil();
+  Future<int> getPageCount(int pageSize) {
+    return Future.value((_organizations.length / pageSize).ceil());
   }
 
   @override
-  UnmodifiableListView<Organization> getOrganizations(
+  Future<UnmodifiableListView<Organization>> getOrganizations(
     int pageNumber,
     int pageSize,
   ) {
-    return UnmodifiableListView<Organization>(
-      _organizations
-          .getRange((pageNumber - 1) * pageSize, pageNumber * pageSize),
-    );
-  }
+    return Future.sync(() {
+      int start = (pageNumber - 1) * pageSize;
+      int end = start + pageSize;
 
-  @override
-  void add(Organization organization) {
-    _organizations.add(organization);
-    notifyListeners();
-  }
-
-  @override
-  void remove(Organization organization) {
-    _organizations.remove(organization);
-    notifyListeners();
-  }
-
-  @override
-  void update(Organization organization) {
-    for (var org in _organizations) {
-      if (org.id == organization.id) {
-        org.name = organization.name;
-        org.description = organization.description;
-        org.openForDonations = organization.openForDonations;
-        org.donations.clear();
-        org.donations.addAll(organization.donations);
-        break;
+      if (start >= _organizations.length) {
+        return UnmodifiableListView<Organization>([]);
+      } else if (end > _organizations.length) {
+        return UnmodifiableListView(_organizations.skip(start));
+      } else {
+        return UnmodifiableListView(_organizations.getRange(start, end));
       }
-    }
+    });
+  }
 
-    notifyListeners();
+  @override
+  Future<void> add(Organization organization) {
+    return Future.sync(() {
+      _organizations.add(organization);
+      notifyListeners();
+    });
+  }
+
+  @override
+  Future<void> remove(Organization organization) {
+    return Future.sync(() {
+      _organizations.remove(organization);
+      notifyListeners();
+    });
+  }
+
+  @override
+  Future<void> update(Organization organization) {
+    return Future.sync(() {
+      for (var org in _organizations) {
+        if (org.id == organization.id) {
+          org.name = organization.name;
+          org.description = organization.description;
+          org.openForDonations = organization.openForDonations;
+          org.donations.clear();
+          org.donations.addAll(organization.donations);
+          break;
+        }
+      }
+
+      notifyListeners();
+    });
   }
 }
