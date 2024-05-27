d1f7311944ecefa5c63dd48ab96c33bbe9f03e2b
John Rufo de la Peña <jodelapena2@up.edu.ph>
2024-05-27 16:34:09 +0800

    Create FirebaseStorageProvider

---
+++ lib/api/firebase_storage_api.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:final_proj/providers/cloud_storage_provider.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path_provider/path_provider.dart';
+
+class FirebaseStorageProvider extends CloudStorageProvider {
+  final FirebaseStorage _storage;
+
+  FirebaseStorageProvider(this._storage);
+
+  @override
+  Future<String> uploadFile(File file) async {
+    return await _storage
+        .ref()
+        .child(file.path)
+        .putFile(File(file.path))
+        .then((task) => task.ref.fullPath);
+  }
+
+  @override
+  Future<File> pickFile(String cloudPath) async {
+    final ref = _storage
+      .ref(cloudPath);
+
+    final tempDir = await getTemporaryDirectory();
+    final file = File("${tempDir.absolute.path}/${ref.name}");
+
+    if (await file.exists()) {
+      return file;
+    }
+
+    await ref.writeToFile(file);
+    return file;
+  }
+
+  @override
+  Future<void> deleteFile(String cloudPath) async {
+    await _storage
+      .ref(cloudPath)
+      .delete();
+  }
+}
